e28f301ff292b746088fb96f7ff80348dd5245b3
samahhamed227 <samahhamed227@gmail.com>
2021-09-19 20:56:08 +0300

    CardAboutusBySamah

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,6 +2,6 @@ import React, { Component } from 'react';
 
 export default class AboutUs extends Component {
   render() {
-    return <div><h1> About Us Page </h1></div>;
+    return <div></div>;
   }
 }
