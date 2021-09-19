c6f435a32b0e003ab476cf5ea5e3dc5cbf4d167c
jehadabuawwad <jehadabuawwad@outlook.com>
2021-09-19 15:02:05 +0300

    Feedback Form location Changed by Jehad

--- src/components/FeedbackSlider.js
+++ src/components/FeedbackSlider.js
@@ -1,11 +1,7 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class FeedbackSlider extends Component {
   render() {
-    return (
-      <div>
-        
-      </div>
-    )
+    return <div></div>;
   }
 }
