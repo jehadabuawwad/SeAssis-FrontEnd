1dbe6a3a7a0872692035a502f5a40fe2a459213c
samahhamed227 <samahhamed227@gmail.com>
2021-09-19 14:21:34 +0300

    CreatFooterBySamah

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,15 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import { Navbar } from "react-bootstrap";
 export default class Footer extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div style={{ textAlign: "center" }}>
+        <Navbar bg="light" variant="light">
+          <Navbar.Brand>
+            <p  style={{ fontSize:'14px' }}>Code-Vibes_Developer&copy; 2021</p>
+          </Navbar.Brand>
+        </Navbar>
+      </div>
+    );
   }
 }
