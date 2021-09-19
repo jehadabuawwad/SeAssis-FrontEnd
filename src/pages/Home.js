2729e212e05e20df9d668c1a810ec9c62e5609e0
samahhamed227 <samahhamed227@gmail.com>
2021-09-19 10:12:59 +0300

    CreatHomeSlideBySamah

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
+import HomeSlider from "../components/HomeSlider";
 export default class Home extends Component {
   render() {
-    return <div><h1> Home Page </h1></div>;
+    return (
+      <div>
+        <h1> Home Page </h1>
+
+        <HomeSlider />
+      </div>
+    );
   }
 }
