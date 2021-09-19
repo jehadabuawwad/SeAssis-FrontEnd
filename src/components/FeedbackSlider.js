e23f3582e0c575c5c896c75f9adb17a2e3c1fb4a
samahhamed227 <samahhamed227@gmail.com>
2021-09-19 15:24:31 +0300

    SliderFeedBySamah

--- src/components/FeedbackSlider.js
+++ src/components/FeedbackSlider.js
@@ -1,7 +1,35 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import Carousel from 'react-bootstrap/Carousel';
 export default class FeedbackSlider extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div style={{ marginBottom: 50 }}>
+        <Carousel ariant="dark">
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="http://via.placeholder.com/640x360"
+              alt="First slide"
+              fluid
+            />
+            <Carousel.Caption>
+              <h1 style={{ color: "white" }}>Road Service</h1>
+              <p style={{ color: "white" }}>test1 </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="http://via.placeholder.com/640x360"
+              alt="Second slide"
+            />
+            <Carousel.Caption>
+              <h1 style={{ color: "white" }}>Home Service</h1>
+              <p style={{ color: "white" }}>test2</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </div>
+    );
   }
 }
